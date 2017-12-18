4c194565be5b0f3fff6f5e90272dde59d0c40f9c
highcoder1 <15295812124@163.com>
2017-12-18 16:17:07 +0800

    修改项目结构

---
+++ src/components/home.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>Home页</div>
+</template>
+
+
+<script>
+    export default {
+        
+    }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,12 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false//阻止vue在启动时生成生产模式下的提示
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  template: '<App/>',
-  components: { App }
+  render: h => h(App)//vue1.0写法： components: { App }  局部注册组件
 })
